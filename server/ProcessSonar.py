cc79fa0356e2c5d82df5ce4d275e852a5bbe946a
cx33 <chengkang.xu@duke.edu>
2018-01-29 20:40:09 -0500

    sub category added

--- server/ProcessSonar.py
+++ server/ProcessSonar.py
@@ -84,12 +84,27 @@ class ProcessSonar (object):
         	'squid:S2388', 'squid:S2386', 'squid:S2387',
         	'squid:S2384', 'squid:S2141', 'squid:S3038',
         	'squid:S2156'}
-        Modularbility_sub = []
+        Modularbility_sub = ["Tell, don't ask: classes should be responsible for their own data and delegate to other objects instead of doing it themselves",
+                             "No public instance variables: keep implementation details of your class hidden from the public interface",
+                             'No "manager" classes: create several classes that work together distributing intelligence, rather than one "smart" class and a few "dumb" helpers',
+                             'No static variables: there should be no reason for shared global public state',
+                             'Active classes: classes should not consist of only get/set methods and, in general, should minimize their use. ',
+                             'get methods should give away the minimal information possible',
+                             'set methods should validate data received',
+                             'Superclasses are their own class: thus should not contain instance variables or methods specific to only some subclasses']
+
            #Flexibility
         Flexibility = {'common-java:DuplicatedBlocks', 'squid:S3047', 'squid:S3776',
         	'squid:S2176', 'squid:MethodCyclomaticComplexity', 'squid:S138',
         	'squid:S1067', 'squid:S1479', 'squid:S1118',
         	'squid:S00107', 'squid:S3422', 'squid:S2166'}
+
+        Flexibility_sub = ['DRY: no duplicated code, either exactly (from cutting and pasting), structurally (in flow of control or decisions), or in setup ("boilerplate" code)',
+                           'Declared types should be as general as possible (i.e., ArrayList should never be visible in your public interface)',
+                           'Single Purpose: keep classes, methods, and variables short and well named by giving them only one purpose',
+                           'Behavior Driven Design: give each class a purpose by focusing on the behavior (or services) it provides first, its state later',
+                           'Polymorphism: use subclassing to avoid "case-based code logic" (i.e., conditional chains or case statements on "type" information)']
+
           #Java related
         JavaNote = {}
           #code smell
