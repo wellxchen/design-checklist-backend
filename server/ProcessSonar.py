a611517d54df907bf192041459455172584c886d
cx33 <chengkang.xu@duke.edu>
2018-01-31 10:55:56 -0500

    fix schema

--- server/ProcessSonar.py
+++ server/ProcessSonar.py
@@ -159,68 +159,68 @@ class ProcessSonar (object):
 
         data['error']['Communication']['Concise code'] = {}
         data['error']['Communication']['Concise code']["category description"] = categories().Communication_Sub[6]
-        data['error']['Communication']['Concise code']["message"] = self.message[0][5]
+        data['error']['Communication']['Concise code']["detail"] = self.message[0][5]
 
         data['error']['Communication']['No warning'] = {}
         data['error']['Communication']['No warning']["category description"] = categories().Communication_Sub[7]
-        data['error']['Communication']['No warning']["message"] = self.message[0][6]
+        data['error']['Communication']['No warning']["detail"] = self.message[0][6]
 
         data['error']['Modularity'] = {}
         data['error']['Modularity']['Data responsibility'] = {}
         data['error']['Modularity']['Data responsibility']["category description"]  = categories().Modularity_sub[1]
-        data['error']['Modularity']['Data responsibility']["message"] = self.message[1][0]
+        data['error']['Modularity']['Data responsibility']["detail"] = self.message[1][0]
 
         data['error']['Modularity']['No public instance variables'] = {}
         data['error']['Modularity']['No public instance variables']["category description"] = categories().Modularity_sub[2]
-        data['error']['Modularity']['No public instance variables']["message"] = self.message[1][1]
+        data['error']['Modularity']['No public instance variables']["detail"] = self.message[1][1]
 
         data['error']['Modularity']['No manager classes'] = {}
         data['error']['Modularity']['No manager classes']["category description"] = categories().Modularity_sub[3]
-        data['error']['Modularity']['No manager classes']["message"] = self.message[1][2]
+        data['error']['Modularity']['No manager classes']["detail"] = self.message[1][2]
 
         data['error']['Modularity']['No static variables'] = {}
         data['error']['Modularity']['No static variables']["category description"] = categories().Modularity_sub[4]
-        data['error']['Modularity']['No static variables']["message"] = self.message[1][3]
+        data['error']['Modularity']['No static variables']["detail"] = self.message[1][3]
 
         data['error']['Modularity']['Active classes'] = {}
         data['error']['Modularity']['Active classes']["category description"] = categories().Modularity_sub[5]
-        data['error']['Modularity']['Active classes']["message"] = self.message[1][4]
+        data['error']['Modularity']['Active classes']["detail"] = self.message[1][4]
 
         data['error']['Modularity']['Get method give minimum info'] = {}
         data['error']['Modularity']['Get method give minimum info']["category description"] = categories().Modularity_sub[6]
-        data['error']['Modularity']['Get method give minimum info']["message"] = self.message[1][5]
+        data['error']['Modularity']['Get method give minimum info']["detail"] = self.message[1][5]
 
         data['error']['Modularity']['Get method validate input'] = {}
         data['error']['Modularity']['Get method validate input']["category description"] = categories().Modularity_sub[7],
-        data['error']['Modularity']['Get method validate input']["message"] = self.message[1][6]
+        data['error']['Modularity']['Get method validate input']["detail"] = self.message[1][6]
 
         data['error']['Modularity']['Superclasses are their own class'] = {}
         data['error']['Modularity']['Superclasses are their own class']["category description"] = categories().Modularity_sub[8]
-        data['error']['Modularity']['Superclasses are their own class']["message"] =  self.message[1][7]
+        data['error']['Modularity']['Superclasses are their own class']["detail"] =  self.message[1][7]
 
         data['error']['Flexibility'] = {}
         data['error']['Flexibility']['No duplicated code'] = {}
         data['error']['Flexibility']['No duplicated code']["category description"] = categories().Modularity_sub[1]
-        data['error']['Flexibility']['No duplicated code']["message"] = self.message[2][0]
+        data['error']['Flexibility']['No duplicated code']["detail"] = self.message[2][0]
 
         data['error']['Flexibility']['General type'] = {}
         data['error']['Flexibility']['General type']["category description"] = categories().Modularity_sub[2]
-        data['error']['Flexibility']['General type']["message"] = self.message[2][1]
+        data['error']['Flexibility']['General type']["detail"] = self.message[2][1]
 
         data['error']['Flexibility']['Single Purpose'] = {}
         data['error']['Flexibility']['Single Purpose']["category description"] = categories().Modularity_sub[3]
-        data['error']['Flexibility']['Single Purpose']["message"] =  self.message[2][2]
+        data['error']['Flexibility']['Single Purpose']["detail"] =  self.message[2][2]
 
         data['error']['Flexibility']['Behavior Driven Design'] = {}
         data['error']['Flexibility']['Behavior Driven Design']["category description"] = categories().Modularity_sub[4]
-        data['error']['Flexibility']['Behavior Driven Design']["message"] = self.message[2][3]
+        data['error']['Flexibility']['Behavior Driven Design']["detail"] = self.message[2][3]
 
         data['error']['Flexibility']['Polymorphism'] = {}
         data['error']['Flexibility']['Polymorphism']["category description"] = categories().Modularity_sub[5]
-        data['error']['Flexibility']['Polymorphism']["message"] = self.message[2][4]
+        data['error']['Flexibility']['Polymorphism']["detail"] = self.message[2][4]
 
-        data['error']['Java Note'] = self.message[3]
-        data['error']['Code Smell'] = self.message[4]
+        data['error']['Java Notes'] = self.message[3]
+        data['error']['Code Smells'] = self.message[4]
         data['percentage'] = {}
         data['percentage']['Communication'] = percentageA
         data['percentage']['Modularity'] = percentageB
