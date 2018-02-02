4e3f73929f2d822fdee0acf56fd2310c3f2dc279
cx33 <chengkang.xu@duke.edu>
2018-02-01 23:44:48 -0500

    refactor code

--- server/ProcessSonar.py
+++ server/ProcessSonar.py
@@ -32,9 +32,6 @@ class ProcessSonar (object):
             for j in range(k):
                 self.message[i].append([])
 
-
-
-
     def checkRuleID(self, ruleID, errmessage):
         ruleInfo = categories().getRuleDetail(ruleID)
 
@@ -57,19 +54,13 @@ class ProcessSonar (object):
                 return (0.0 + len(category) - len(rules_under_category)) / len(category) * 100.00
         return 100.0
 
-    def errHandler (self):
-        data = {}
-        data['err'] = "project not found"
-        data['description'] = "please change the file name and extension for xml.txt to pom.xml and yml.txt to .gitlab-ci.yml"
-        return json.dumps(data)
-
     def process(self):
 
         #if project not been analysis return error
         r = requests.get(self.SONAR_URL + "/api/components/show?component=" + self.TEST_PROJECT)
         found_project = r.json()
         if 'errors' in found_project:
-            return self.errHandler()
+            return utility().errHandler()
 
         #get number of pages
         r = requests.get(
@@ -127,112 +118,17 @@ class ProcessSonar (object):
 
                 self.checkRuleID (ruleID, errmessage)
 
-
-
         #cal percentage
-        percentageA = self.calpercentage(categories().communication, self.rulesViolated[0])
-        percentageB = self.calpercentage(categories().modularity, self.rulesViolated[1])
-        percentageC = self.calpercentage(categories().flexibility, self.rulesViolated[2])
-        percentageD = self.calpercentage(categories().javanote, self.rulesViolated[3])
-        percentageE = self.calpercentage(categories().codesmell, self.rulesViolated[4])
+        percentage = []
+        percentage.append(self.calpercentage(categories().communication, self.rulesViolated[0]))
+        percentage.append(self.calpercentage(categories().modularity, self.rulesViolated[1]))
+        percentage.append(self.calpercentage(categories().flexibility, self.rulesViolated[2]))
+        percentage.append(self.calpercentage(categories().javanote, self.rulesViolated[3]))
+        percentage.append(self.calpercentage(categories().codesmell, self.rulesViolated[4]))
 
-
-        data = {}
-        data['error'] = {}
-        data['error']['Communication'] = {}
-        data['error']['Communication']['Meaningful names'] = {}
-        data['error']['Communication']['Meaningful names']["category description"] = categories().Communication_Sub[1]
-        data['error']['Communication']['Meaningful names']["detail"]  = self.message[0][0]
-
-        data['error']['Communication']['No magic values'] = {}
-        data['error']['Communication']['No magic values']["category description"] = categories().Communication_Sub[2]
-        data['error']['Communication']['No magic values']["detail"] =  self.message[0][1]
-
-        data['error']['Communication']['Readable code'] = {}
-        data['error']['Communication']['Readable code']["category description"] = categories().Communication_Sub[3]
-        data['error']['Communication']['Readable code']["detail"] = self.message[0][2]
-
-        data['error']['Communication']['Use scope wisely'] = {}
-        data['error']['Communication']['Use scope wisely']["category description"] = categories().Communication_Sub[4]
-        data['error']['Communication']['Use scope wisely']["detail"] = self.message[0][3]
-
-        data['error']['Communication']['Same level code'] = {}
-        data['error']['Communication']['Same level code']["category description"] = categories().Communication_Sub[5]
-        data['error']['Communication']['Same level code']["detail"] = self.message[0][4]
-
-        data['error']['Communication']['Concise code'] = {}
-        data['error']['Communication']['Concise code']["category description"] = categories().Communication_Sub[6]
-        data['error']['Communication']['Concise code']["detail"] = self.message[0][5]
-
-        data['error']['Communication']['No warning'] = {}
-        data['error']['Communication']['No warning']["category description"] = categories().Communication_Sub[7]
-        data['error']['Communication']['No warning']["detail"] = self.message[0][6]
-
-        data['error']['Modularity'] = {}
-        data['error']['Modularity']['Data responsibility'] = {}
-        data['error']['Modularity']['Data responsibility']["category description"]  = categories().Modularity_sub[1]
-        data['error']['Modularity']['Data responsibility']["detail"] = self.message[1][0]
-
-        data['error']['Modularity']['No public instance variables'] = {}
-        data['error']['Modularity']['No public instance variables']["category description"] = categories().Modularity_sub[2]
-        data['error']['Modularity']['No public instance variables']["detail"] = self.message[1][1]
-
-        data['error']['Modularity']['No manager classes'] = {}
-        data['error']['Modularity']['No manager classes']["category description"] = categories().Modularity_sub[3]
-        data['error']['Modularity']['No manager classes']["detail"] = self.message[1][2]
-
-        data['error']['Modularity']['No static variables'] = {}
-        data['error']['Modularity']['No static variables']["category description"] = categories().Modularity_sub[4]
-        data['error']['Modularity']['No static variables']["detail"] = self.message[1][3]
-
-        data['error']['Modularity']['Active classes'] = {}
-        data['error']['Modularity']['Active classes']["category description"] = categories().Modularity_sub[5]
-        data['error']['Modularity']['Active classes']["detail"] = self.message[1][4]
-
-        data['error']['Modularity']['Get method give minimum info'] = {}
-        data['error']['Modularity']['Get method give minimum info']["category description"] = categories().Modularity_sub[6]
-        data['error']['Modularity']['Get method give minimum info']["detail"] = self.message[1][5]
-
-        data['error']['Modularity']['Get method validate input'] = {}
-        data['error']['Modularity']['Get method validate input']["category description"] = categories().Modularity_sub[7],
-        data['error']['Modularity']['Get method validate input']["detail"] = self.message[1][6]
-
-        data['error']['Modularity']['Superclasses are their own class'] = {}
-        data['error']['Modularity']['Superclasses are their own class']["category description"] = categories().Modularity_sub[8]
-        data['error']['Modularity']['Superclasses are their own class']["detail"] =  self.message[1][7]
-
-        data['error']['Flexibility'] = {}
-        data['error']['Flexibility']['No duplicated code'] = {}
-        data['error']['Flexibility']['No duplicated code']["category description"] = categories().Modularity_sub[1]
-        data['error']['Flexibility']['No duplicated code']["detail"] = self.message[2][0]
-
-        data['error']['Flexibility']['General type'] = {}
-        data['error']['Flexibility']['General type']["category description"] = categories().Modularity_sub[2]
-        data['error']['Flexibility']['General type']["detail"] = self.message[2][1]
-
-        data['error']['Flexibility']['Single Purpose'] = {}
-        data['error']['Flexibility']['Single Purpose']["category description"] = categories().Modularity_sub[3]
-        data['error']['Flexibility']['Single Purpose']["detail"] =  self.message[2][2]
-
-        data['error']['Flexibility']['Behavior Driven Design'] = {}
-        data['error']['Flexibility']['Behavior Driven Design']["category description"] = categories().Modularity_sub[4]
-        data['error']['Flexibility']['Behavior Driven Design']["detail"] = self.message[2][3]
-
-        data['error']['Flexibility']['Polymorphism'] = {}
-        data['error']['Flexibility']['Polymorphism']["category description"] = categories().Modularity_sub[5]
-        data['error']['Flexibility']['Polymorphism']["detail"] = self.message[2][4]
-
-        data['error']['Java Notes'] = self.message[3]
-        data['error']['Code Smells'] = self.message[4]
-        data['percentage'] = {}
-        data['percentage']['Communication'] = percentageA
-        data['percentage']['Modularity'] = percentageB
-        data['percentage']['Flexibility'] = percentageC
-        data['percentage']['Java Notes'] = percentageD
-        data['percentage']['Code Smells'] = percentageE
-
+        data = utility.dataHandler(self.message, percentage)
         res = json.dumps(data,
-                 indent=4, separators=(',', ': '))
+                        indent=4, separators=(',', ': '))
 
 
         return res
