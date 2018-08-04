7bc661169c804c85e854cc71f8aa8399fdaf48a8
wellxchen <xu411@purdue.edu>
2018-08-04 14:47:41 +0800

    all info from code maat included

--- server/shell/code_maat.sh
+++ server/shell/code_maat.sh
@@ -18,5 +18,10 @@ git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renam
 
 cd $DEPENDENCY_ROOT
 
+java -jar code-maat-1.1-SNAPSHOT-standalone.jar -l $LOGPATH/code_maat.log -c git2 
 java -jar code-maat-1.1-SNAPSHOT-standalone.jar -l $LOGPATH/code_maat.log -c git2 -a summary
-
+java -jar code-maat-1.1-SNAPSHOT-standalone.jar -l $LOGPATH/code_maat.log -c git2 -a coupling
+java -jar code-maat-1.1-SNAPSHOT-standalone.jar -l $LOGPATH/code_maat.log -c git2 -a abs-churn
+java -jar code-maat-1.1-SNAPSHOT-standalone.jar -l $LOGPATH/code_maat.log -c git2 -a author-churn
+java -jar code-maat-1.1-SNAPSHOT-standalone.jar -l $LOGPATH/code_maat.log -c git2 -a entity-ownership
+java -jar code-maat-1.1-SNAPSHOT-standalone.jar -l $LOGPATH/code_maat.log -c git2 -a entity-effort
